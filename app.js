34e3bfb404ad62491fee2b979e86be57efeebc9f
DouDouLee <lixia0001@hotmail.com>
2016-12-10 15:25:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -50,3 +50,4 @@ http.listen(appEnv.port, "0.0.0.0", function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url + " " + appEnv.port);
 });
+
